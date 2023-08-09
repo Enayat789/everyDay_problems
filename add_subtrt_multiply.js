909625eb1fb5c1c4726a3b0b5c83ae09e8af9c6b
enayat789 <enayatbhai2006@gmail.com>
2023-08-09 22:38:20 +0530

    it is a function that can perform addition, subtraction, and multiplication of two numbers

--- add_subtrt_multiply.js
+++ add_subtrt_multiply.js
@@ -1,20 +1,29 @@
 // Make a  Js function that can perform addition, subtraction, and multiplication of two numbers:
 
 function add_subtrt_multiply(num1, num2) {
+  var all_result = [];
   var a = num1;
   b = num2;
   for (let i = 1; i <= 1; i++) {
     const sum = a + b;
-    console.log("Addition of " + a + " + " + b + " is " + sum);
+    all_result.push("Addition of " + a + " + " + b + " is " + sum);
+    // console.log("Addition of " + a + " + " + b + " is " + sum);
     for (let j = 0; j < 1; j++) {
       const subtract = a - b;
-      console.log("Subtraction of " + a + " - " + b + " is " + subtract);
+      all_result.push("Subtraction of " + a + " - " + b + " is " + subtract);
+      // console.log("Subtraction of " + a + " - " + b + " is " + subtract);
       for (let k = 0; k < 1; k++) {
         const multiply = a * b;
-        console.log("Multiplication of " + a + " x " + b + " is " + multiply);
+        all_result.push(
+          "Multiplication of " + a + " x " + b + " is " + multiply
+        );
+        // console.log("Multiplication of " + a + " x " + b + " is " + multiply);
       }
     }
   }
+
+  return all_result;
 }
 
-add_subtrt_multiply(2, 4);
+const result = add_subtrt_multiply(2, 4);
+console.log(result);
