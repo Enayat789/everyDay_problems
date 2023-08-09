c4ca859e1e6d0b26149e4f332f2af2645bc43b4e
enayat789 <enayatbhai2006@gmail.com>
2023-08-09 22:26:51 +0530

    a program that prints numbers from 1 to 100,but for multiples of 3, print Fizz, for multiples of 5,print Buzz, and for multiples of both 3 and 5, print FizzBuzz.

--- printFizzBuzz_Numbers.js
+++ printFizzBuzz_Numbers.js
@@ -1,17 +1,24 @@
-// .FizzBuzz: Write a program that prints numbers from 1 to 100, but for multiples of 3, print "Fizz," for multiples of 5, print "Buzz," and for multiples of both 3 and 5, print "FizzBuzz."
+// .FizzBuzz: Write a program that prints numbers from 1 to 100,
+// but for multiples of 3, print "Fizz," for multiples of 5,
+//print "Buzz," and for multiples of both 3 and 5, print "FizzBuzz."
 
-function printNumbers_hundred(numLength) {
-  for (let i = 1; i <= numLength; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-      console.log("Fizz");
-    } else if (i % 5 == 0) {
-      console.log("Buzz");
-    } else {
-      console.log(i);
+function printNumbers_hundred(arrayLength) {
+  const forFizz = [];
+  const forBuzz = [];
+  const forFizzBuzz = [];
+
+  for (let i = 1; i <= arrayLength; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      forFizzBuzz.push(i);
+    } else if (i % 3 === 0) {
+      forFizz.push(i);
+    } else if (i % 5 === 0) {
+      forBuzz.push(i);
     }
   }
+  return { forFizz, forBuzz, forFizzBuzz };
 }
 
-printNumbers_hundred(30);
+const result = printNumbers_hundred(100);
+
+console.log(result);
