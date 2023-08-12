7dffd679d45faa265db6f4cd27999663bc2e7034
enayat789 <enayatbhai2006@gmail.com>
2023-08-12 10:54:13 +0530

    It will add two numbers as string

---
+++ addTwo_string.js
@@ -0,0 +1,17 @@
+// Create a function that will add two positive numbers of indefinite size.
+// The numbers are received as strings and the result should be also provided as string.
+
+function add_string(string1, string2) {
+  const added_string = [];
+
+  const str1 = string1;
+  const str2 = string2;
+
+  const sum = "This is the sum of two string numbers: " + str1 + str2;
+  added_string.push(sum);
+
+  return added_string;
+}
+
+const result = add_string(4, 6);
+console.log(result);
