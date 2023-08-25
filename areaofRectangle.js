eaf320f2226bd50178065cd295bbfb9af69e84cc
enayat789 <enayatbhai2006@gmail.com>
2023-08-25 16:36:05 +0530

    find area of a rectangle

---
+++ areaofRectangle.js
@@ -0,0 +1,8 @@
+// find area of a rectangle
+
+function areaofRectangle(length, breadth) {
+  const area = length * breadth;
+  return area;
+}
+const result = areaofRectangle(3, 5);
+console.log(result);
